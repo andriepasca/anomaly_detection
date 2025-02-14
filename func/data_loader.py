49ac385c3d6342f5adce4e6b48c4c7593156f33d
andriepasca <88814716+andriepasca@users.noreply.github.com>
2025-02-14 10:07:57 +0100

    Update data_loader.py

--- func/data_loader.py
+++ func/data_loader.py
@@ -2,16 +2,25 @@ import pandas as pd
 import numpy as np
 
 
-def data_load(url):
-    data = pd.read_csv(url)
+def data_load(path, format):
+    if format == 'csv':
+        data = pd.read_csv(path)
+    elif format == 'xlsx':
+        data = pd.read_excel(path)
     return data
 
-def df_load(df,x_column,y_column):
+def df_load(df,x_column,y_column, ctx):
     df[x_column] = df[x_column].astype(str)
     df[x_column] = pd.to_datetime(df[x_column], utc=True)
     df = df.set_index(x_column)
-    # df.drop(columns=x_column,inplace=True)
+    default_interval = df.index.inferred_freq
+    if default_interval == None:
+        df = df.groupby(df.index).mean()
+        df = df.asfreq(ctx['interval'])
+        df = df.resample(ctx['interval']).mean()
     df[y_column] = df[y_column].replace('',np.nan).astype(float)
     df[y_column] = df[y_column].interpolate()
     df[y_column] = df[y_column].bfill()
+
+    
     return df
