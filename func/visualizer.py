aef9100bb867dbd1b39b2e4a7ed0b860f4f3f37d
andriepasca <88814716+andriepasca@users.noreply.github.com>
2025-02-14 10:08:14 +0100

    Update visualizer.py

--- func/visualizer.py
+++ func/visualizer.py
@@ -6,20 +6,8 @@ def plot_base_line(df,y_column):
     figure.add_trace(go.Scatter(name=y_column, x = df.index, y=df[y_column], marker=dict(color='rgba(50,50,50,0.3)')))
     return figure
 
-def plot_anomalies(df, y_column, anomaly_type):
+def plot_anomalies(df, y_column, config):
     figure = go.Figure()
-    if anomaly_type == 'seasonal':
-        config={
-            'anomaly_column':'seasonal_ad',
-            'legend_name': 'seasonal anomaly',
-            'color':'rgba(249,123,34,0.8)'
-        }
-    elif anomaly_type == 'isolationforest':
-        config={
-            'anomaly_column':'isolation_ad',
-            'legend_name': 'collective anomaly',
-            'color':'rgba(255,217,61,0.8)'
-        }
     # plot baseline
     figure.add_trace(go.Scatter(name=y_column, x = df.index, y=df[y_column], marker=dict(color='rgba(50,50,50,0.3)')))
 
